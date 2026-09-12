b13d0d4bcc76d682ea1e9de3ea050177227052b6
Sebastian Putra <sebasputraa@gmail.com>
2026-09-12 13:23:20 +0700

    Kembalikan nama ke Stephen Septian setelah tes editor

--- stephen-bni/data.js
+++ stephen-bni/data.js
@@ -14,7 +14,7 @@ var DATA = {
     "web": "increasink.co.id"
   },
   "hero": {
-    "name": "test 123",
+    "name": "Stephen Septian",
     "initials": "SS",
     "role_id": "Founder & Creative Director, Increasink",
     "role_en": "Founder & Creative Director, Increasink",
